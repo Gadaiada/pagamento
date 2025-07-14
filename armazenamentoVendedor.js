48bd762140c9bdced0b35ca2b1630712c1787094
Gadaiada <gadaiada.cadastro@gmail.com>
2025-07-14 15:14:15 -0400

    Update armazenamentoVendedor.js

--- armazenamentoVendedor.js
+++ armazenamentoVendedor.js
@@ -1,28 +1,19 @@
-const fs = require('fs');
-const CAMINHO = './vendedores.json';
+const vendedores = {};
 
-function carregar() {
-  try {
-    return JSON.parse(fs.readFileSync(CAMINHO));
-  } catch {
-    return {};
-  }
+function salvarVendedorTemporario(idCliente, dados) {
+  vendedores[idCliente] = dados;
 }
 
-function salvar(dados) {
-  fs.writeFileSync(CAMINHO, JSON.stringify(dados, null, 2));
+function buscarVendedor(idCliente) {
+  return vendedores[idCliente];
+}
+
+function aprovarVendedor(idCliente) {
+  if (vendedores[idCliente]) vendedores[idCliente].aprovado = true;
 }
 
 module.exports = {
-  salvarVendedorTemporario: (id, dados) => {
-    const json = carregar();
-    json[id] = { ...dados, status: 'aguardando' };
-    salvar(json);
-  },
-  aprovarVendedor: (id) => {
-    const json = carregar();
-    if (json[id]) json[id].status = 'aprovado';
-    salvar(json);
-  },
-  buscarVendedor: (id) => carregar()[id]
+  salvarVendedorTemporario,
+  buscarVendedor,
+  aprovarVendedor
 };
