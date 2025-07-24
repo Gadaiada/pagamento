0e82362574855f35f7512b1bf843b69a8710f150
Gadaiada <gadaiada.cadastro@gmail.com>
2025-07-24 16:20:45 -0400

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,8 @@
+module.exports.gerarSenhaAleatoria = function gerarSenhaAleatoria(tamanho = 8) {
+  const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let senha = '';
+  for (let i = 0; i < tamanho; i++) {
+    senha += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+  }
+  return senha;
+};
